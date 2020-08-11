09337bba0aeac3f188241a6dd3e505dc145fc8c5
shahrya <64373780+shahrya@users.noreply.github.com>
2020-08-11 19:53:01 +0500

    Add files via upload

---
+++ QuizApp/y/app.js
@@ -0,0 +1,166 @@
+function check(){
+    var score = 0
+    var rightAnswer1 = document.getElementById("Q.1-A.1")
+    var q1_answer_2 = document.getElementById("Q.1-A.2") 
+    var q1_answer_3 = document.getElementById("Q.1-A.3")
+    var q1_answer_4 = document.getElementById("Q.1-A.2")
+
+    if(rightAnswer1.checked == true){
+        score++
+        alert("Q.No.1 is Right")
+    }
+    else{
+        alert("Q.No.1 is Wrong")
+    }
+
+
+    var rightAnswer2 = document.getElementById("Q.2-A.1")
+    var q2_answer_2 = document.getElementById("Q.2-A.2")
+    var q2_answer_3 = document.getElementById("Q.2-A.3")
+    var q2_answer_4 = document.getElementById("Q.2-A.4")
+
+    if(rightAnswer2.checked == true){
+        score++
+        alert("Q.No.2 is Right")
+    }
+    else{
+        alert("Q.No.2 is Wrong")
+    }
+
+     
+    var rightAnswer3 = document.getElementById("Q.3-A.1")
+    var q3_answer_2 = document.getElementById("Q.3-A.2")
+    var q3_answer_3 = document.getElementById("Q.3-A.3")
+    var q3_answer_4 = document.getElementById("Q.3-A.4")
+
+    if(rightAnswer3.checked == true){
+        score++
+        alert("Q.No.3 is Right")
+    }
+    else{
+        alert("Q.No.3 is Wrong")
+    }
+
+    
+
+    var rightAnswer4 = document.getElementById("Q.4-A.1")
+    var q4_answer_2 = document.getElementById("Q.4-A.2")
+    var q4_answer_3 = document.getElementById("Q.4-A.3")
+    var q4_answer_4 = document.getElementById("Q.4-A.4")
+
+    if(rightAnswer4.checked == true){
+        score++
+        alert("Q.No.4 is Right")
+    }
+    else{
+        alert("Q.No.4 is Wrong")
+    }
+
+
+
+    var rightAnswer5 = document.getElementById("Q.5-A.1")
+    var q5_answer_2 = document.getElementById("Q.5-A.2")
+    var q5_answer_3 = document.getElementById("Q.5-A.3")
+    var q5_answer_4 = document.getElementById("Q.5-A.4")
+
+    if(rightAnswer5.checked == true){
+        score++
+        alert("Q.No.5 is Right")
+    }
+    else{
+        alert("Q.No.5 is Wrong")
+    }
+
+    var rightAnswer6 = document.getElementById("Q.6-A.1")
+    var q6_answer_2 = document.getElementById("Q.6-A.2")
+    var q6_answer_3 = document.getElementById("Q.6-A.3")
+    var q6_answer_4 = document.getElementById("Q.6-A.4")
+
+    if(rightAnswer6.checked == true){
+        score++
+        alert("Q.No.6 is Right")
+    }
+    else{
+        alert("Q.No.6 is Wrong")
+    }
+
+
+    var rightAnswer7 = document.getElementById("Q.7-A.1")
+    var q7_answer_2 = document.getElementById("Q.7-A.2")
+    var q7_answer_3 = document.getElementById("Q.7-A.3")
+    var q7_answer_4 = document.getElementById("Q.7-A.4")
+
+    if(rightAnswer7.checked == true){
+        score++
+        alert("Q.No.7 is Right")
+    }
+    else{
+        alert("Q.No.7 is Wrong")
+    }
+
+
+
+    var rightAnswer8 = document.getElementById("Q.8-A.1")
+    var q8_answer_2 = document.getElementById("Q.8-A.2")
+    var q8_answer_3 = document.getElementById("Q.8-A.3")
+    var q8_answer_4 = document.getElementById("Q.8-A.4")
+
+    if(rightAnswer8.checked == true){
+        score++
+        alert("Q.No.8 is Right")
+    }
+    else{
+        alert("Q.No.8 is Wrong")
+    }
+
+
+
+    
+    var rightAnswer9 = document.getElementById("Q.9-A.1")
+    var q9_answer_2 = document.getElementById("Q.9-A.2")
+    var q9_answer_3 = document.getElementById("Q.9-A.3")
+    var q9_answer_4 = document.getElementById("Q.9-A.4")
+
+    if(rightAnswer9.checked == true){
+        score++
+        alert("Q.No.9 is Right")
+    }
+    else{
+        alert("Q.No.9 is Wrong")
+    }
+
+
+
+     
+    var rightAnswer10 = document.getElementById("Q.10-A.1")
+    var q10_answer_2 = document.getElementById("Q.10-A.2")
+    var q10_answer_3 = document.getElementById("Q.10-A.3")
+    var q10_answer_4 = document.getElementById("Q.10-A.4")
+
+    if(rightAnswer10.checked == true){
+        score++
+        alert("Q.No.10 is Right")
+    }
+    else{
+        alert("Q.No.10 is Wrong")
+    }
+
+
+
+   alert("Your total score is " + " " + score++ + " " + "out of 10")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
